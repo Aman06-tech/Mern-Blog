965ee31293072cdf14e55c81e88356d4f058e188
Aman06-tech <210160203026.aman@gdgu.org>
2024-07-02 18:12:01 +0530

    create navbar using flowbite

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-function App() {  
-  return (
-    <div className='flex h-screen w-screen '>
-      <p className='text-red-500'>hello</p>
-    </div>
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import Header from "./components/Header";
+
+ 
+export default function App() {  
+  return ( 
+    <BrowserRouter>
+    <Header  />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
+      <Route path="/dashboard" element={<Dashboard/>} />
+      <Route path="/projects" element={<Projects/>} />
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
-export default App
+
